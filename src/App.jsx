f6823e57924900a7482d18c735e38dabb1ed496e
Giovanna de Moraes <gigimoraessol@gmail.com>
2025-04-06 11:10:31 -0300

    add json-server

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,28 @@ const url = "http://localhost:3000/products";
 function App() {
   const [products, setProducts] = useState([]);
 
-  useEffect(async () => {
-    const res = await fetch(url);
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch(url);
 
-    const data = await res.json();
+      const data = await res.json();
 
-    setProducts(data);
+      setProducts(data);
+    }
+
+    fetchData();
   }, []);
-
-  console.log(products);
 
   return (
     <div className="App">
       <h1>Lista de produtos </h1>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            {product.name} - R$: {product.price}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
