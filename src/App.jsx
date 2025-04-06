858faa7597db1491112c81f65945658eb854735b
Giovanna de Moraes <gigimoraessol@gmail.com>
2025-04-06 17:07:01 -0300

    importando o hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,30 @@
 import { useState, useEffect } from "react";
 import "./App.css";
 
+import { useFetch } from "./hooks/useFetch";
+
 const url = "http://localhost:3000/products";
 
 function App() {
   const [products, setProducts] = useState([]);
 
+  const { data: items } = useFetch(url);
+
   const [name, setName] = useState("");
 
   const [price, setPrice] = useState("");
 
-  useEffect(() => {
-    async function fetchData() {
-      const res = await fetch(url);
+  // useEffect(() => {
+  //   async function fetchData() {
+  //     const res = await fetch(url);
 
-      const data = await res.json();
+  //     const data = await res.json();
 
-      setProducts(data);
-    }
+  //     setProducts(data);
+  //   }
 
-    fetchData();
-  }, []);
+  //   fetchData();
+  // }, []);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -50,11 +54,12 @@ function App() {
     <div className="App">
       <h1>Lista de produtos </h1>
       <ul>
-        {products.map((product) => (
-          <li key={product.id}>
-            {product.name} - R$: {product.price}
-          </li>
-        ))}
+        {items &&
+          items.map((product) => (
+            <li key={product.id}>
+              {product.name} - R$: {product.price}
+            </li>
+          ))}
       </ul>
       <div className="add-product">
         <form onSubmit={handleSubmit}>
