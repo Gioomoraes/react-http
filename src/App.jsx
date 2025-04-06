296d4085730f39c44cbcfd2be029cccaebbeb101
Giovanna de Moraes <gigimoraessol@gmail.com>
2025-04-06 10:17:24 -0300

    resgatando dados

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0);
+const url = "http://localhost:3000/products";
 
-  return <></>;
+function App() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(async () => {
+    const res = await fetch(url);
+
+    const data = await res.json();
+
+    setProducts(data);
+  }, []);
+
+  console.log(products);
+
+  return (
+    <div className="App">
+      <h1>Lista de produtos </h1>
+    </div>
+  );
 }
 
 export default App;
